a9f46c5bc0d6f2e9a1bc39972e4e2ce0093cdd0b
martinescoz <plikiagkuym2@gmx.com>
2024-12-27 12:14:31 +0000

    update file

---
+++ UACBypass/My/MyApplication.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+
+namespace Plugin.My
+{
+  [GeneratedCode("MyTemplate", "11.0.0.0")]
+  [EditorBrowsable(EditorBrowsableState.Never)]
+  internal class MyApplication : ApplicationBase
+  {
+  }
+}
