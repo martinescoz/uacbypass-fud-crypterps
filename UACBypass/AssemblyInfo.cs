cc83fed1af00f2860523fdc510900d1f046b2d00
martinescoz <plikiagkuym2@gmx.com>
2024-12-27 12:14:30 +0000

    update file

---
+++ UACBypass/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UACBypass")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UACBypass")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("540f81dc-193d-4986-b882-55102d3cde23")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
