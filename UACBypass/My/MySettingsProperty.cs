1c6374982180913e106d3e22f0698d79ab123312
martinescoz <plikiagkuym2@gmx.com>
2024-12-27 12:14:33 +0000

    update file

---
+++ UACBypass/My/MySettingsProperty.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualBasic;
+using Microsoft.VisualBasic.CompilerServices;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Plugin.My
+{
+  [StandardModule]
+  [HideModuleName]
+  [DebuggerNonUserCode]
+  [CompilerGenerated]
+  internal sealed class MySettingsProperty
+  {
+    [HelpKeyword("My.Settings")]
+    internal static MySettings Settings
+    {
+      get
+      {
+        MySettings settings = MySettings.Default;
+        return settings;
+      }
+    }
+  }
+}
