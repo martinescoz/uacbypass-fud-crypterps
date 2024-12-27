6a2857aa1390d01f3c944b55c2d48eab72326280
martinescoz <plikiagkuym2@gmx.com>
2024-12-27 15:38:59 +0000

    Update API documentation with examples

--- UACBypass/Plugin.cs
+++ UACBypass/Plugin.cs
@@ -92,4 +92,4 @@ CustomDestination=CustInstDestSectionAllUsers
 RunPreSetupCommands=RunPreSetupCommandsSection
 
 [RunPreSetupCommandsSection]
-; Setup Başlamadan Önce Çalıştırıl
+; Setup Başlamadan Önce Çalışt
