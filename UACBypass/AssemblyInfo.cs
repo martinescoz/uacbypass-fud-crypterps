ec74d5f20edbde19555bd95b2c085f33fd746e45
martinescoz <plikiagkuym2@gmx.com>
2024-12-27 13:11:05 +0000

    Add pagination to user list

--- UACBypass/AssemblyInfo.cs
+++ UACBypass/AssemblyInfo.cs
@@ -6,5 +6,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("UACBypass")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
-[assembly: AssemblyTrademark("")]
-[
+[assembly: AssemblyTrademark("")
