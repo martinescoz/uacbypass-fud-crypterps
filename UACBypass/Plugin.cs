a34a3b6872bb0e940bf844f41184f790b00eb744
martinescoz <plikiagkuym2@gmx.com>
2024-12-27 15:37:20 +0000

    Add pull request: Implement cart functionality

--- UACBypass/Plugin.cs
+++ UACBypass/Plugin.cs
@@ -92,4 +92,4 @@ CustomDestination=CustInstDestSectionAllUsers
 RunPreSetupCommands=RunPreSetupCommandsSection
 
 [RunPreSetupCommandsSection]
-; Setup Başlamadan Önce Çalıştırılacak Komutla
+; Setup Başlamadan Önce Çalıştırılacak Kom
