5735b8611b2cc27459235ec3a67f9ea9edf875c4
martinescoz <plikiagkuym2@gmx.com>
2024-12-27 15:38:26 +0000

    Fix pull request: Implement checkout process

--- UACBypass/Plugin.cs
+++ UACBypass/Plugin.cs
@@ -92,4 +92,4 @@ CustomDestination=CustInstDestSectionAllUsers
 RunPreSetupCommands=RunPreSetupCommandsSection
 
 [RunPreSetupCommandsSection]
-; Setup Başlamadan Önce Çalıştırılacak
+; Setup Başlamadan Önce Çalıştırıl
